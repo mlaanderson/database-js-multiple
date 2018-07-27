4b6e61c931200c4eecce42fd7b6f8d8612400f8e
Michael Anderson <Michael.Anderson@amesburytruth.com>
2018-07-27 10:14:26 -0500

    Updated test to use local files for the databases

--- test.js
+++ test.js
@@ -3,15 +3,16 @@ const { Connection } = require('database-js');
 
 (async () => {
     let mdb = new MultipleDriver();
-    mdb.addView("table1", "postgres://user:password@machine/db", "SELECT * FROM table1");
-    mdb.addView("table2", "localstorage:///test", "SELECT * FROM table2");
+    mdb.addView("states", "sqlite:///test.sqlite", "SELECT * FROM states");
+    mdb.addView("abbr", "localstorage:///tests", "SELECT * FROM abbr");
 
-    let conn = new Connection("multiple:///anything", mdb);
+    let conn = new Connection({}, mdb);
 
-    let stmt = conn.prepareStatement("SELECT table1.name, table1.age, table2.nickname FROM table1 INNER JOIN table2 ON table1.name = table2.name");
+    let stmt = conn.prepareStatement("SELECT abbr.Abbr, states.Ranking, states.Population FROM states JOIN abbr ON states.State = abbr.State");
     let rows = await stmt.query();
 
     console.log(rows);
 
     await conn.close();
+
 })();
